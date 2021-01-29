32fcb8fa6129e70823bc65b0c4764116073dbfc8
Juan Landron <41309063+JuanLandron0@users.noreply.github.com>
2021-01-29 15:59:55 -0500

    Create Read.php
    Preliminary read API

---
+++ LAMPAPI/Read.php
@@ -0,0 +1,65 @@
+<?php
+
+    $inData = getRequestInfo();
+
+    $username = "";
+    $password = "";
+    
+    $fistname = "";
+    $lastname = "";
+
+    $conn = new mysqli("localhost", "admin", "admin", "COP4331");
+    if($conn->connection_error)
+    {
+        returnWithError($conn->connection_error);
+    }
+    else
+    {
+        $sql = "SELECT firstname FROM Contacts WHERE firstname like '%" . $inData["firstname"] 
+            . "%' and lastname like '%" . $inData["lastname"] . "%'";
+        $result = $conn->query($sql);
+        if($result->num_rows > 0)
+        {
+            while($row = $result->fetch_assoc())
+            {
+                if($searchCount > 0)
+                {
+                    $searchResults .= ",";
+                }
+                $searchCount++;
+                $searchResults .= '"' . $row["firstname"] . '"';
+            }
+        }
+        else
+        {
+            returnWithError("No Contacts Found");
+        }
+        $conn->close();
+    }
+
+    returnWithInfo($searchResults);
+
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson($obj)
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+
+    function returnWithError($err)
+    {
+        $retValue = '{"id":0,"firstname":"","lastname":"","error":"' . $err . '"}';
+        sendResultInfoAsJson($retValue);
+    }
+
+    function returnWithInfo($searchResults)
+    {
+        $retValue = '{"results":[' . $searchResults . '],"error":""';
+        sendResultInfoAsJson($retValue);
+    }
+
+?>
