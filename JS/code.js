5378cb47c4fa99634a0e60c6f7f6d05c1c94af0c
FreddieFrat27 <70779198+FreddieFrat27@users.noreply.github.com>
2021-02-02 17:34:55 -0500

    Updated functionality and merged all JS.

---
+++ JS/code.js
@@ -0,0 +1,217 @@
+var urlBase = 'http://cop4331-15.tech/LAMPAPI';
+var extension = 'php';
+
+var userId = 0;
+var firstName = "";
+var lastName = "";
+
+function register()
+{
+	{
+		var login = document.getElementById("registerEmail").value;
+		var password = document.getElementById("registerPassword").value;
+		var first = document.getElementById("registerFirst").value;
+		var last = document.getElementById("registerLast").value;
+		var hash = md5( password );
+		
+		//document.getElementById("loginResult").innerHTML = "";
+	
+		var jsonPayload = '{"firstName" : "' + first + '", "lastName" : "' + last + '", "login" : "' + login + '", "password" : "' + hash + '"}';
+		var url = urlBase + '/Register.' + extension;
+	
+		console.log(jsonPayload);
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", url, true);
+		xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+		try
+		{
+			xhr.onreadystatechange = function()
+			{
+				if (this.readyState == 4 && this.status == 200)
+				{
+					window.location.href = "index.html";
+				}
+			};
+			xhr.send(jsonPayload);
+		}
+		catch(err)
+		{
+			document.getElementById("registerResult").innerHTML = err.message;
+		}
+	
+	}
+}	
+
+function login()
+{
+	userId = 0;
+	firstName = "";
+	lastName = "";
+	
+	var login = document.getElementById("loginName").value;
+	var password = document.getElementById("loginPassword").value;
+	var hash = md5( password );
+	
+	document.getElementById("loginResult").innerHTML = "";
+
+	var jsonPayload = '{"login" : "' + login + '", "password" : "' + hash + '"}';
+	var url = urlBase + '/Login.' + extension;
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, false);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.send(jsonPayload);
+		
+		var jsonObject = JSON.parse( xhr.responseText );
+		
+		userId = jsonObject.id;
+		
+		if( userId < 1 )
+		{
+			document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+			return;
+		}
+		
+		firstName = jsonObject.firstName;
+		lastName = jsonObject.lastName;
+
+		saveCookie();
+	
+		window.location.href = "index.html";
+	}
+	catch(err)
+	{
+		document.getElementById("loginResult").innerHTML = err.message;
+	}
+
+}
+
+function saveCookie()
+{
+	var minutes = 20;
+	var date = new Date();
+	date.setTime(date.getTime()+(minutes*60*1000));	
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+}
+
+function readCookie()
+{
+	userId = -1;
+	var data = document.cookie;
+	var splits = data.split(",");
+	for(var i = 0; i < splits.length; i++) 
+	{
+		var thisOne = splits[i].trim();
+		var tokens = thisOne.split("=");
+		if( tokens[0] == "firstName" )
+		{
+			firstName = tokens[1];
+		}
+		else if( tokens[0] == "lastName" )
+		{
+			lastName = tokens[1];
+		}
+		else if( tokens[0] == "userId" )
+		{
+			userId = parseInt( tokens[1].trim() );
+		}
+	}
+	
+	if( userId < 0 )
+	{
+		window.location.href = "index.html";
+	}
+	else
+	{
+		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+	}
+}
+
+function Logout()
+{
+	userId = 0;
+	firstName = "";
+	lastName = "";
+	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+	window.location.href = "index.html";
+}
+
+function createContact()
+{
+	var first = document.getElementById("createFirst");
+	var last = document.getElementById("createLast");
+	var phone = document.getElementById("createPhone");
+	var address = document.getElementById("createAddress");
+
+	document.getElementById("contactAddResult").innerHTML = "";
+
+	var jsonPayload = '{"first" : "' + first + '", "last" : "' + last + '", "phone" : "' + phone + '", "address" : "' + address + '"}';
+
+	var url = urlBase + '/Add.' + extension;
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function()
+		{
+			if (this.readyState == 4 && this.status == 200)
+			{
+				window.location.href = "contacts.html";
+			}
+		};
+
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("contactAddResult").innerHTML = err.message;
+	}
+}
+
+// Could potentially use to search for contacts
+// function searchUser()
+// {
+// 	var srch = document.getElementById("searchText").value;
+// 	document.getElementById("colorSearchResult").innerHTML = "";
+	
+// 	var colorList = "";
+	
+// 	var jsonPayload = '{"search" : "' + srch + '","userId" : ' + userId + '}';
+// 	var url = urlBase + '/SearchColors.' + extension;
+	
+// 	var xhr = new XMLHttpRequest();
+// 	xhr.open("POST", url, true);
+// 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+// 	try
+// 	{
+// 		xhr.onreadystatechange = function() 
+// 		{
+// 			if (this.readyState == 4 && this.status == 200) 
+// 			{
+// 				document.getElementById("colorSearchResult").innerHTML = "Color(s) has been retrieved";
+// 				var jsonObject = JSON.parse( xhr.responseText );
+				
+// 				for( var i=0; i<jsonObject.results.length; i++ )
+// 				{
+// 					colorList += jsonObject.results[i];
+// 					if( i < jsonObject.results.length - 1 )
+// 					{
+// 						colorList += "<br />\r\n";
+// 					}
+// 				}
+				
+// 				document.getElementsByTagName("p")[0].innerHTML = colorList;
+// 			}
+// 		};
+// 		xhr.send(jsonPayload);
+// 	}
+// 	catch(err)
+// 	{
+// 		document.getElementById("colorSearchResult").innerHTML = err.message;
+// 	}
+	
+// }
