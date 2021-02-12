78f55809b544989f3474508b6c03b066c9b49152
FreddieFrat27 <70779198+FreddieFrat27@users.noreply.github.com>
2021-02-12 13:17:18 -0500

    Updated search

--- JS/code.js
+++ JS/code.js
@@ -179,46 +179,43 @@ function newContact()
 	window.location.href = "add.html";
 }
 
-// Could potentially use to search for contacts
-// function searchUser()
-// {
-// 	var srch = document.getElementById("searchText").value;
-// 	document.getElementById("colorSearchResult").innerHTML = "";
+function search()
+{
+	var srch = document.getElementById("searchText").value;
+	document.getElementById("searchResult").innerHTML = "";
 
-// 	var colorList = "";
+	var jsonPayload = '{"search" : "' + srch + '","userId" : ' + userId + '}';
+	var url = urlBase + '/Read.' + extension;
 
-// 	var jsonPayload = '{"search" : "' + srch + '","userId" : ' + userId + '}';
-// 	var url = urlBase + '/SearchColors.' + extension;
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function()
+		{
+			if (this.readyState == 4 && this.status == 200)
+			{
+				document.getElementById("searchResult").innerHTML = "User found";
+				var jsonObject = JSON.parse( xhr.responseText );
 
-// 	var xhr = new XMLHttpRequest();
-// 	xhr.open("POST", url, true);
-// 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-// 	try
-// 	{
-// 		xhr.onreadystatechange = function()
-// 		{
-// 			if (this.readyState == 4 && this.status == 200)
-// 			{
-// 				document.getElementById("colorSearchResult").innerHTML = "Color(s) has been retrieved";
-// 				var jsonObject = JSON.parse( xhr.responseText );
+				for( var i=0; i<jsonObject.results.length; i++ )
+				{
+					contactList += jsonObject.results[i];
+					if( i < jsonObject.results.length - 1 )
+					{
+						contactList += "<br />\r\n";
+					}
+				}
 
-// 				for( var i=0; i<jsonObject.results.length; i++ )
-// 				{
-// 					colorList += jsonObject.results[i];
-// 					if( i < jsonObject.results.length - 1 )
-// 					{
-// 						colorList += "<br />\r\n";
-// 					}
-// 				}
+				document.getElementsByTagName("p")[0].innerHTML = contactList;
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("searchResult").innerHTML = err.message;
+	}
 
-// 				document.getElementsByTagName("p")[0].innerHTML = colorList;
-// 			}
-// 		};
-// 		xhr.send(jsonPayload);
-// 	}
-// 	catch(err)
-// 	{
-// 		document.getElementById("colorSearchResult").innerHTML = err.message;
-// 	}
-
-// }
+}
