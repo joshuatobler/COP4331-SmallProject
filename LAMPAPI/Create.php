b3e42e019d13f896b79ae39d947bb723dcaf0bd1
Joshua Tobler <76630638+joshuatobler@users.noreply.github.com>
2021-02-13 14:10:19 -0500

    Update Create.php

--- LAMPAPI/Create.php
+++ LAMPAPI/Create.php
@@ -5,13 +5,11 @@ $post_data = get_json_request();
 $conn = new mysqli("localhost", "admin", "admin", "COP4331");
 if (!$conn->connect_error) {
     $sql = "insert into contacts (foreignid, firstname, lastname, email, phone) VALUES
-            ('" . $post_data["id"] . "', '" . $post_data["first"] . "', '" . $post_data["last"] . "', '" . $post_data["email"] . "', '" . $post_data["phone"] . "')"; //line 17 is going to change based on how the database is updated
-
-    if ($result = $conn->query($sql) !== TRUE) {
+            ('" . $post_data["id"] . "', '" . $post_data["first"] . "', '" . $post_data["last"] . "', '" . $post_data["email"] . "', '" . $post_data["phone"] . "')";
+    if ($result = $conn->query($sql) !== true) {
         send_json_response($conn->error, true, 500);
     }
-
-    send_json_response('User successfully created');
+    send_json_response('User inserted successfully');
     $conn->close();
 } else {
     send_json_response($conn->connect_error, true, 500);
@@ -34,13 +32,11 @@ function get_json_request()
 function send_json_response($res, $err = false, $res_code = null)
 {
     header('Content-type: application/json');
-
     if ($res_code === null) {
         http_response_code($err ? 500 : 200);
     } else {
         http_response_code($res_code);
     }
-
     $json_response = json_encode([
         'status' => $err ? 'failure' : 'success',
         'message' => $res
