a58821a16f9d5800f420f394565d5ea596cbeb44
Joshua Tobler <76630638+joshuatobler@users.noreply.github.com>
2021-02-13 13:40:02 -0500

    Update Register.php

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -1,49 +1,51 @@
 <?php
 
-    $inData = getRequestInfo();
-    
-    $firstname = $inData["firstname"];
-    $lastname = $inData["lastname"];
-    $email = $inData["email"];
-    $password = $inData["password"];
+$post_data = get_json_request();
 
-    $conn = new mysqli("localhost", "admin", "admin", "COP4331");
-    if($conn->connect_error)
-    {
-        returnWithError($conn->connect_error);
+$conn = new mysqli("localhost", "admin", "admin", "COP4331");
+if (!$conn->connect_error) {
+    $sql = "insert into user (password, firstname, lastname, email) VALUES
+            ('" . $post_data["password"] . "', '" . $post_data["firstname"] . "', '" . $post_data["lastname"] . "', '" . $post_data["email"] . "')";
+    if ($result = $conn->query($sql) !== true) {
+        send_json_response($conn->error, true, 500);
     }
-    else
-    {
-        $sql = "insert into user (password, firstname, lastname, email) VALUES
-            ('" . $password . "', '" . $firstname . "', '" . $lastname . "', '" . $email ."')";
-        if($result = $conn->query($sql) != TRUE){
-            returnWithError($conn->error);
+    send_json_response('User inserted successfully');
+    $conn->close();
+} else {
+    send_json_response($conn->connect_error, true, 500);
+}
+
+function get_json_request()
+{
+    $json_str = file_get_contents('php://input');
+    if ($json_str !== false) {
+        $json_arr = json_decode($json_str, true);
+        if ($json_arr !== null) {
+            return $json_arr;
         }
-        returnWithInfo(); // checks to see if it ended up working
-        $conn->close();
     }
 
-    function getRequestInfo()
-    {
-        return json_decode(file_get_contents('php://input'), true);
-    }
+    send_json_response('Input not in JSON format', true, 400);
+    exit();
+}
 
-    function sendResultInfoAsJson($obj)
-    {
-        header('Content-type: application/json');
-        echo $obj;
+function send_json_response($res, $err = false, $res_code = null)
+{
+    header('Content-type: application/json');
+    if ($res_code === null) {
+        http_response_code($err ? 500 : 200);
+    } else {
+        http_response_code($res_code);
     }
+    $json_response = json_encode([
+        'status' => $err ? 'failure' : 'success',
+        'message' => $res
+    ]);
 
-    function returnWithError($err)
-    {
-        $retValue = '{"password":"", "firstname":"", "lastname":"", "email":"","error":"' . $err . '"}';
-        sendResultInfoAsJson($retValue);
+    if ($json_response !== false) {
+        echo $json_response;
+    } else {
+        http_response_code(500);
+        exit();
     }
-
-    function returnWithInfo() // added function for successful response
-    {
-        $retValue = '{"info":"Success"}';
-        sendResultInfoAsJson( $retValue );
-    }
-    
-?>
+}
