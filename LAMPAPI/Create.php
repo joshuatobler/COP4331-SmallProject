26eb4d1106ea5641bea8b9c57f9d27b01be6e748
Joshua Tobler <76630638+joshuatobler@users.noreply.github.com>
2021-02-13 13:40:31 -0500

    Update Create.php

--- LAMPAPI/Create.php
+++ LAMPAPI/Create.php
@@ -1,49 +1,55 @@
 <?php
 
-    $inData = getRequestInfo();
+$post_data = get_json_request();
 
-    $firstname = $inData["firstname"];
-    $lastname = $inData["lastname"];
-    $email = $inData["email"];
-    $phone = $inData["phone"]; // added phone
+$conn = new mysqli("localhost", "admin", "admin", "COP4331");
+if (!$conn->connect_error) {
+    $sql = "INSERT INTO contacts (foreignid, firstname, lastname, email, phone) VALUES
+            ('" . $post_data["id"] . "', '" . $post_data["first"] . "', '" . $post_data["last"] . "', '" . $post_data["email"] . "', '" . $post_data["phone"] . "')"; //line 17 is going to change based on how the database is updated
 
-    $conn = new mysqli("localhost", "admin", "admin", "COP4331");
-    if($conn->connect_error)
-    {
-        returnWithError($conn->connect_error);
+    if ($result = $conn->query($sql) !== TRUE) {
+        send_json_response($conn->error, true, 500);
     }
-    else
-    {
-        $sql = "insert into contacts (firstname, lastname, email, phone) VALUES
-            ('" . $firstname . "', '" . $lastname . "', '" . $email . "', '" . $phone . "')"; // added phone
-        if($result = $conn->query($sql) != TRUE){
-            returnWithError($conn->error);
+
+    send_json_response('User successfully created');
+    $conn->close();
+} else {
+    send_json_response($conn->connect_error, true, 500);
+}
+
+function get_json_request()
+{
+    $json_str = file_get_contents('php://input');
+    if ($json_str !== false) {
+        $json_arr = json_decode($json_str, true);
+        if ($json_arr !== null) {
+            return $json_arr;
         }
-        returnWithInfo(); //added to check for success
-        $conn->close();
     }
 
-    function getRequestInfo()
-    {
-        return json_decode(file_get_contents('php://input'), true);
+    send_json_response('Input not in JSON format', true, 400);
+    exit();
+}
+
+function send_json_response($res, $err = false, $res_code = null)
+{
+    header('Content-type: application/json');
+
+    if ($res_code === null) {
+        http_response_code($err ? 500 : 200);
+    } else {
+        http_response_code($res_code);
     }
 
-    function sendResultInfoAsJson($obj)
-    {
-        header('Content-type: application/json');
-        echo $obj;
-    }
+    $json_response = json_encode([
+        'status' => $err ? 'failure' : 'success',
+        'message' => $res
+    ]);
 
-    function returnWithError($err)
-    {
-        $retValue = '{"firstname":"", "lastname":"","error":"' . $err . '"}';
-        sendResultInfoAsJson($retValue);
+    if ($json_response !== false) {
+        echo $json_response;
+    } else {
+        http_response_code(500);
+        exit();
     }
-    
-    function returnWithInfo() // added function for successful response
-    {
-        $retValue = '{"info":"Success"}';
-        sendResultInfoAsJson( $retValue );
-    }
-
-?>
+}
