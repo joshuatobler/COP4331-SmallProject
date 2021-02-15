96e3716508f3084ec0ac916506b5441c943718f1
Seth-Brockett <69112750+Seth-Brockett@users.noreply.github.com>
2021-02-15 11:15:13 -0800

    Create ReadAll.php

---
+++ LAMPAPI/ReadAll.php
@@ -0,0 +1,56 @@
+<?php
+
+$post_data = get_json_request();
+$searchResults = [];
+
+$conn = new mysqli("localhost", "admin", "admin", "COP4331");
+if (!$conn->connection_error) {
+    $sql = "SELECT id FROM contacts WHERE foreignid='" . $post_data["id"] . "'";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while($row =  $result->fetch_assoc()){
+            $searchResults[] = $row;
+        }
+        send_json_response($searchResults);
+    } else {
+        send_json_response("No Contacts Found", true, 200);
+    }
+    $conn->close();
+} else {
+    send_json_response($conn->connect_error, true, 500);
+}
+
+function get_json_request()
+{
+    $json_str = file_get_contents('php://input');
+    if ($json_str !== false) {
+        $json_arr = json_decode($json_str, true);
+        if ($json_arr !== null) {
+            return $json_arr;
+        }
+    }
+
+    send_json_response('Input not in JSON format', true, 400);
+    exit();
+}
+
+function send_json_response($res, $err = false, $res_code = null)
+{
+    header('Content-type: application/json');
+    if ($res_code === null) {
+        http_response_code($err ? 500 : 200);
+    } else {
+        http_response_code($res_code);
+    }
+    $json_response = json_encode([
+        'status' => $err ? 'failure' : 'success',
+        'message' => $res
+    ]);
+
+    if ($json_response !== false) {
+        echo $json_response;
+    } else {
+        http_response_code(500);
+        exit();
+    }
+}
